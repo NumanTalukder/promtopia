eb033b12445fb96cbfb6aed3af925451dd5fcc47
unknown <numantalukder1001@gmail.com>
2023-06-10 20:09:47 +0600

    navbar update

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,6 +7,19 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 const Nav = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true)
 
+  const [providers, serProviders] = useState(null)
+  const [toggleDropdown, setToggleDropdown] = useState(false)
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders()
+
+      setProviders(response)
+    }
+
+    setProviders()
+  }, [])
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
@@ -20,7 +33,7 @@ const Nav = () => {
         <p className='logo_text'>Promptopia</p>
       </Link>
 
-      {/* mobile navigation */}
+      {/* desktop navigation */}
       <div className='sm:flex hidden'>
         {isLoggedIn ? (
           <div className='flex gap-3 md:gap-5'>
@@ -43,7 +56,58 @@ const Nav = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className='sm:hidden flex relative'>
+        {isLoggedIn ? (
+          <div className='flex'>
+            <Image
+              src='/assets/images/logo.svg'
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='profile'
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+            {toggleDropdown && (
+              <Link
+                href='/profile'
+                className='dropdown_link'
+                onClick={setToggleDropdown((prev) => !prev)}
+              >
+                My Profile
+              </Link>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type='button'
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className='black_btn'
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
